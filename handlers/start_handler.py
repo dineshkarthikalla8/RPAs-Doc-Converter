b38cb202b54a73b6eb3bc4ca501a4aee3c97df2e
Alla Dinesh Karthik <dineshkarthikalla@gmail.com>
2026-03-27 19:53:03 +0530

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,18 +1,22 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
+from functions.user_store import save_user   # 👈 ADD THIS
 
-# Keyboard buttons
 keyboard = [
     ["Merge PDF", "Split PDF"],
     ["Compress PDF", "PDF to Word"],
-    ["Word to PDF", "PDF to JPG"],["JPG to PDF", "Feedback/Suggestion"]
+    ["Word to PDF", "PDF to JPG"],
+    ["JPG to PDF", "Feedback/Suggestion"]
 ]
 
 reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 
-# Start command handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    
+    user_id = update.effective_user.id
+    save_user(user_id)   # 👈 STORE USER
+
     await update.message.reply_text(
         "📂 RPA DOC CONVERTER\nSelect a tool:",
         reply_markup=reply_markup
