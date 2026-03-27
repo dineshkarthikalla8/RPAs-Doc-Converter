e222de880eaecb69f06d9e09cb39d3658e67cdfc
Alla Dinesh Karthik <dineshkarthikalla@gmail.com>
2026-03-27 21:30:14 +0530

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,6 +1,8 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
 from functions.user_store import save_user   # 👈 ADD THIS
+from functions.user_store import add_user
+
 
 keyboard = [
     ["Merge PDF", "Split PDF"],
@@ -21,3 +23,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "📂 RPA DOC CONVERTER\nSelect a tool:",
         reply_markup=reply_markup
     )
+async def start(update, context):
+    user = update.effective_user
+    add_user(user)
+
+    await update.message.reply_text("Welcome to DOC Converter Bot 🚀")
